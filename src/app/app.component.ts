50eed2d907e2f07b591b57110a5f75938cd9b474
ismail RZOUKI <ismailrzouki7@gmail.com>
2024-09-23 19:51:39 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SaleComponent } from './sale/sale.component';
 
 @Component({
   selector: 'app-root',
