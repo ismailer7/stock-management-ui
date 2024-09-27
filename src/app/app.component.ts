3a89774ed27185739c9f2ea35fd4a6bfba6c3f3c
medisensei <medios1596@gmail.com>
2024-09-27 22:38:49 +0100

    page login routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
 import {NavbarComponent} from './components/layaout/navbar/navbar.component';
 import {LayaoutComponent} from "./components/layaout/layaout.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, LayaoutComponent],
+  imports: [NavbarComponent, LayaoutComponent,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,26 @@ import { ProductComponent } from './components/product/product.component';
 import { AccountComponent } from './components/account/account.component';
 import { SaleComponent } from './components/sale/sale.component';
 import { CategoryComponent } from './components/category/category.component';
+import { LoginComponent } from './components/login/login.component';
+import { LayaoutComponent } from './components/layaout/layaout.component';
+import { LoginLayoutComponent } from './data/login-layout/login-layout.component';
 
 export const routes: Routes = [
+    {
+    path: '',
+    component: LoginLayoutComponent,
+    children: [
+    { path: 'login', component: LoginComponent },
+    { path: '', redirectTo: 'login', pathMatch: 'full' }
+    ]},
+    {
+    path: '',
+    component: LayaoutComponent,
+    children: [
     { path: 'dashboard', component: DashboardComponent, data: { name: 'Dashboard'} },
     { path: 'products', component: ProductComponent, data: { name: 'Products'} },
     { path: 'category', component: CategoryComponent, data: { name: 'Category'} },
     { path: 'sales', component: SaleComponent, data: { name: 'Sales'} },
-    { path: 'account', component: AccountComponent, data: { name: 'Account'} }
+    { path: 'account', component: AccountComponent, data: { name: 'Account'} } 
+]}
 ];
