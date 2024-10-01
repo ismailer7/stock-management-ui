5958941227addd57f60217a785a9ffd66a25a84f
ismail RZOUKI <ismailrzouki7@gmail.com>
2024-10-01 02:49:06 +0200

    fix footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
 import {NavbarComponent} from './components/layaout/navbar/navbar.component';
 import {LayaoutComponent} from "./components/layaout/layaout.component";
+import { FooterComponent } from './components/layaout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, LayaoutComponent],
+  imports: [NavbarComponent, LayaoutComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
