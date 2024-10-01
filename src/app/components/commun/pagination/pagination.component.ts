543bebb75c031218444f64beeecd553fc89866be
ismail RZOUKI <ismailrzouki7@gmail.com>
2024-10-02 00:58:26 +0200

    fix transaltion

--- src/app/components/commun/pagination/pagination.component.ts
+++ src/app/components/commun/pagination/pagination.component.ts
@@ -1,9 +1,10 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-pagination',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './pagination.component.html',
   styleUrl: './pagination.component.css'
 })
