907dbea45a69cf65866862624b32b2e548d12843
muhsiine <muhsiine@gmail.com>
2024-10-06 07:29:13 +0100

    fix some styling problems

--- src/app/interceptor/loading.interceptor.ts
+++ src/app/interceptor/loading.interceptor.ts
@@ -3,11 +3,15 @@ import { finalize, Observable } from 'rxjs';
 
 import { inject } from '@angular/core';
 import { LoadingService } from '../services/loading.service';
+import {environment} from "../../environments/environment";
 
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
 
   const loadingService = inject(LoadingService);
-  loadingService.show();
+  const exclusions = [`${environment.rooturl}/product/filtred`,
+    `${environment.rooturl}/sale/filtred`]
+  if(!exclusions.includes(req.url))
+    loadingService.show();
 
   return next(req).pipe(
     finalize(() => loadingService.hide())
