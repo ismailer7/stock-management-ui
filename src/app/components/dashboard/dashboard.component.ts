1b64de4da695527ee0fb07b6297fc1f7626cb117
medisensei <medios1596@gmail.com>
2024-12-01 14:05:13 +0100

    cards V2

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,13 @@
 import { Component, inject } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { TranslateModule, TranslatePipe } from '@ngx-translate/core';
+import { faArrowTrendUp, faCartShopping, faHandHoldingDollar, faSackDollar, faTags} from '@fortawesome/free-solid-svg-icons'
+import { ComponentComunicationService } from '../../services/shared/component-comunication.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule,FontAwesomeModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
@@ -12,6 +15,12 @@ export class DashboardComponent {
 
   private readonly translatePipe = inject(TranslatePipe);
   textTranslatedByPipe = '';
+  compComunicationSrv = inject(ComponentComunicationService);
+  protected readonly faSackDollar = faSackDollar;
+  protected readonly faTags = faTags;
+  protected readonly fafaCartShopping = faCartShopping;
+  protected readonly fafaArrowTrendUp = faArrowTrendUp;
+
 
 someMethod() {
   this.textTranslatedByPipe = this.translatePipe.transform('TRANSLATED_BY_COMPONENT');
